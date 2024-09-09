3a8ea86be7f25ab031212e957fa159ab75d75ae4
Obai Basher <53649412+ObaiBasheer@users.noreply.github.com>
2024-09-09 10:44:01 +0300

    create Integration controller and add Dashboard Method

---
+++ Integration-Hub/Controllers/IntegrationController.cs
@@ -0,0 +1,33 @@
+﻿using Integration_Hub.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Integration_Hub.Controllers
+{
+    public class IntegrationController : Controller
+    {
+        private readonly AppDbContext _context;
+        public IntegrationController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Integration/Dashboard
+        public async Task<IActionResult> Dashboard()
+        {
+            var integrations = await _context.Integrations
+                .Select(i => new IntegrationViewModel
+                {
+                    Id = i.Id,
+                    Name = i.Name,
+                    Type = i.Source.Type.ToString() + " to " + i.Destination.Type.ToString(),
+                    IsActive = i.IsActive,
+                    LastRunTime = i.LastRunTime
+                })
+                .ToListAsync();
+
+            return View(integrations);
+        }
+    }
+}
+}
