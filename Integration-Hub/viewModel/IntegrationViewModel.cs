8f2d62608e47c213fc1d9bb581685c31e8bf756b
Obai Basher <53649412+ObaiBasheer@users.noreply.github.com>
2024-09-09 11:00:25 +0300

    Create Integration View Model

---
+++ Integration-Hub/viewModel/IntegrationViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Integration_Hub.viewModel
+{
+    public class IntegrationViewModel
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Type { get; set; }  // Example: "API to Database"
+        public bool IsActive { get; set; }
+        public DateTime? LastRunTime { get; set; }  // Last time this integration was executed
+    }
+
+}
