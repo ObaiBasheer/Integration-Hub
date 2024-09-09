1206ae138daec9f42fe23ae3f4869ba949154c71
Obai Basher <53649412+ObaiBasheer@users.noreply.github.com>
2024-09-09 10:44:50 +0300

    Config the configration data in AppSitting

--- Integration-Hub/Program.cs
+++ Integration-Hub/Program.cs
@@ -1,8 +1,19 @@
+using Integration_Hub.Config;
+using Integration_Hub.Data;
+using Integration_Hub.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.Configure<IntegrationSettings>(builder.Configuration.GetSection("IntegrationSettings"));
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<IIntegrationService, IntegrationService>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
